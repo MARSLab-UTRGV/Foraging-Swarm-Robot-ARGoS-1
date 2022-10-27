2fd3307e9a5eea6437debcc9358d278b471b3a58
supersweet-dev <jim@supersweet.dev>
2022-10-27 12:59:31 -0500

    cmtd optional `PKG_CONFIG_PATH` in ./CPFA/build.sh

--- CPFA/build.sh
+++ CPFA/build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #if [ "$1" = "clean" ]; then
-    echo "Deleting and recreating the build directory "
-    rm -rf build
-    mkdir build
+echo "Deleting and recreating the build directory "
+rm -rf build
+mkdir build
 #fi
 
 cd build
-export PKG_CONFIG_PATH=/opt/local/argos3/2.8.12.2/gcc/5.4.0/lib/pkgconfig
+#export PKG_CONFIG_PATH=/opt/local/argos3/2.8.12.2/gcc/5.4.0/lib/pkgconfig
 echo "Configuring Makefiles with CMAKE..."
 cmake -DBUILD_EVOLVER=NO -DCMAKE_BUILD_TYPE=Release ..
 
@@ -16,4 +16,3 @@ make
 cd ..
 
 echo "Finished."
-
